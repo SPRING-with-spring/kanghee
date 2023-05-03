24446db7a9fc9fc75215411fc738a89b801ad556
Stellarhalo <stellarhalo@naver.com>
2023-05-03 10:31:35 +0900

    기초세팅

---
+++ src/test/java/com/stellar/coreBasic/CoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stellar.coreBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
